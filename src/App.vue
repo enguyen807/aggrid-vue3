94a039e5e63c8fb00a22662ab344ddd5f1c09d7a
Eric Nguyen <eric@bauwise.com>
2021-08-02 03:49:50 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+  <div>
+    <ag-grid-vue
+      style="width: 100%; height: 500px"
+      class="ag-theme-alpine"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+      :defaultColDef="defaultColDef"
+      editType="fullRow"
+      rowSelection="single"
+      :frameworkComponents="frameworkComponents"
+    >
+    </ag-grid-vue>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { defineComponent, ref, reactive, onBeforeMount } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import { AgGridVue } from "ag-grid-vue3";
+import AgGridSwitch from "@/components/BaseSwitch.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    AgGridVue,
+  },
+  setup() {
+    const columnDefs = ref();
+    const rowData = ref();
+    const defaultColDef = ref();
+
+    const frameworkComponents = reactive({
+      AgGridSwitch,
+    });
+
+    onBeforeMount(() => {
+      columnDefs.value = [
+        { field: "make" },
+        {
+          field: "activate",
+          cellEditorFramework: frameworkComponents.AgGridSwitch,
+        },
+        { field: "model" },
+        { field: "price" },
+      ];
+
+      defaultColDef.value = {
+        flex: 1,
+        editable: true,
+      };
+
+      rowData.value = [
+        { make: "Toyota", activate: 0, model: "Celica", price: 35000 },
+        { make: "Ford", activate: 1, model: "Mondeo", price: 32000 },
+        { make: "Porsche", activate: 0, model: "Boxter", price: 72000 },
+      ];
+    });
+
+    return {
+      columnDefs,
+      defaultColDef,
+      rowData,
+      frameworkComponents,
+    };
+  },
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
---
+++ src/components/BaseSwitch.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="h-100">
+    <!--begin::Switch-->
+    <label
+      id="action-switch-label"
+      class="
+        form-check
+        form-switch
+        form-check-custom
+        form-check-solid
+        form-check-label
+        h-100
+      "
+    >
+      <!--begin::Input-->
+      <input
+        id="action-switch-input"
+        class="form-check-input"
+        name="switch-action"
+        type="checkbox"
+        @click="handleCheckbox"
+        :value="getValue"
+        :checked="switchValue > 0 ? true : false"
+      />
+      <!--end::Input-->
+    </label>
+    <!--end::Switch-->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "AgGridActions",
+  props: ["values"],
+  setup() {
+    const switchValue = ref(0);
+
+    const handleCheckbox = (e) => {
+      if (e.target.checked) {
+        switchValue.value = 1;
+      } else {
+        switchValue.value = 0;
+      }
+    };
+
+    const getValue = () => {
+      return switchValue.value;
+    };
+
+    return {
+      handleCheckbox,
+      getValue,
+    };
+  },
+});
+</script>
+
+<style>
+#action-switch-label {
+  justify-content: center;
+}
+</style>
